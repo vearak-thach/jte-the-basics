a16078345203b06989eb12677e7d994b9d691017
Thach, Vearak (Contractor) <vthach@associates.nsf.gov>
2020-06-23 13:18:56 -0400

    GIT TEST one file

--- libraries/sonarqube/static_code_analysis.groovy
+++ libraries/sonarqube/static_code_analysis.groovy
@@ -2,6 +2,6 @@ void call(){
     stage("SonarQube: Static Code Analysis"){
         println "static code analysis from the sonarqube library"
 
-        println "GIT TEST" 
+        println "GIT TEST edit one file" 
     }
 }
